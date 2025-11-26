afc8bfaf5a594294957085ad6e035cfdb96c572f
pcollas <pcollas-ext@luminess.eu>
2025-11-26 15:39:36 +0000

    update(layout) : change logic and put menu and footer inside app html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
-import { LayoutComponent } from "./core/layout/layout.component";
+import { MenuComponent } from './core/layout/menu/menu.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CoreModule, LayoutComponent],
+  imports: [CoreModule, MenuComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
