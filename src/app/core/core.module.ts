22331e1f83cf944350655b81261a1ec6c27f090d
pcollas <pcollas-ext@luminess.eu>
2025-11-26 15:40:42 +0000

    delete(layout) : change and delete logic for menu, router and footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { LayoutModule } from "./layout/services/layout.module";
-
 @NgModule({
-  imports: [CommonModule, LayoutModule],
-  exports: [LayoutModule]
+  imports: [CommonModule],
+  exports: []
 })
 export class CoreModule {}
